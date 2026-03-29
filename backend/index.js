a32e6536c408447a2bdbacac5aa7819e3d594fb3
Master Shubham <ommylight@gmail.com>
2026-03-29 12:39:17 +0530

    Fix CORS for Vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,29 @@
-require('dotenv').config();   // 👈 TOP pe hona chahiye
+require("dotenv").config();
 
-const connectToMongo = require('./db');
-const express = require('express');
-const cors = require('cors');
+const connectToMongo = require("./db");
+const express = require("express");
+const cors = require("cors");
 
 connectToMongo();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "i-notebook-react-osa1dqttn-master-shubhams-projects.vercel.app", // tumhara exact Vercel URL daalo
+    ],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(PORT, () => {
   console.log(`iNotebook app listening on port ${PORT}`);
 });
-
